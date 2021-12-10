a9ed7dd714ee16afd3296a57340d452283c75bbc
« Matteo <matteo.courquin@gmail.com>
2021-12-10 09:32:42 +0100

    :bug: Add error

--- script.js
+++ script.js
@@ -1,29 +1,58 @@
-let charges = 0
+let charges
+const error = document.querySelector('.champs-error')
 const argentPoche = document.getElementById("total-argent-poche")
 const champsCharges = document.getElementById("total-charges")
 const champsChargesAuto = document.getElementById("total-charges-auto")
 const calcul = document.querySelector('#calculate')
 
+// =========================================================
+
+function isNumber(x) {
+    return !isNaN(parseFloat(x)) && isFinite(x);
+}
+
 function calculArgent() {
-    let inputChargesAuto = document.querySelector("input[name=input-charges-auto]:checked").value;
+    
+    let inputChargesAuto = document.querySelector("input[name=input-charges-auto]:checked").value
 
-    const salaire = document.querySelector('#salaire').value
-    charges = salaire * (inputChargesAuto / 100);
-    let totalArgentPoche = salaire - charges
+    const salaire = document.querySelector('#salaire')
 
-    if (totalArgentPoche <= 0) {
-        argentPoche.style = "color: red;";
-    } else if (totalArgentPoche > 0 && totalArgentPoche <= 700){
-        argentPoche.style = "color: orange;";
-    } else if (totalArgentPoche > 700){
-        argentPoche.style = "color: green;";
+    if (isNumber(salaire.value)) {
+
+        salaire.classList.remove("error");
+
+        error.style = "display: none"
+        champsChargesAuto.style = "display: block"
+        argentPoche.style = "display: block"
+
+        charges = salaire.value * (inputChargesAuto / 100)
+        let totalArgentPoche = salaire.value - charges
+    
+        if (totalArgentPoche <= 0) {
+            argentPoche.style = "color: red;";
+        } else if (totalArgentPoche > 0 && totalArgentPoche <= 700){
+            argentPoche.style = "color: orange;";
+        } else if (totalArgentPoche > 700){
+            argentPoche.style = "color: green;";
+        } else {
+            argentPoche.style = "color: grey;";
+        }
+    
+        champsChargesAuto.textContent = "Charges auto-entrepreneur : " + charges;
+        argentPoche.textContent = "Argent de poche : " + totalArgentPoche;
+        
     } else {
-        argentPoche.style = "color: grey;";
+        salaire.classList.add("error");
+        
+        error.style = "display: block"
+        champsChargesAuto.style = "display: none"
+        argentPoche.style = "display: none"
+
+        error.textContent = "Veuillez entrez un nombre";
     }
 
-    champsChargesAuto.textContent = "Charges auto-entrepreneur : " + charges;
-    argentPoche.textContent = "Argent de poche : " + totalArgentPoche;
-
 }
+
+// =========================================================
 
 calcul.addEventListener('click', calculArgent);
